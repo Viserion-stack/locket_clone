9294b5aa911cedf9a7264c8d35357f474afacb81
Dominik Wawrzonek <dwawrzonek@ics.com>
2022-02-02 16:10:58 +0100

    Service: Add Firebase serive for android % ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:locket_clone/features/presentation/screens/login/bloc/login_bloc.dart';
 import 'package:locket_clone/features/presentation/screens/login/loging_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
