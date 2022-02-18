bb17086ee0abe3017effcdda799388ed050c9150
Dominik Wawrzonek <dwawrzonek@ics.com>
2022-02-18 12:21:12 +0100

    Update: Add FadedOpacity container in introduction screen

--- lib/features/presentation/screens/introduction/introduction_screen.dart
+++ lib/features/presentation/screens/introduction/introduction_screen.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:locket_clone/features/presentation/screens/landing_screen.dart';
 import 'package:locket_clone/features/presentation/widgets/appbar/standard_appbar.dart';
 
 class CheckMyPriceScreen extends StatefulWidget {
@@ -26,9 +27,9 @@ final pagesList = <Widget>[
     appbarItemsColor: Colors.white,
     value: 2,
   ),
-  const _MyBodyWidget(
+  _MyBodyWidget(
     value: 3,
-    color: Colors.black,
+    color: Colors.black87,
     appbarItemsColor: Colors.white,
   ),
 ];
@@ -85,14 +86,26 @@ class _MyBodyWidget extends StatefulWidget {
 }
 
 class _MyBodyWidgetState extends State<_MyBodyWidget>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   AnimationController? controller;
   Animation<double>? annimation;
+  AnimationController? controller1;
+  Animation<double>? annimation1;
 
   List<String> introImages = [
     'assets/images/introImage1.jpeg',
     'assets/images/introImage2.jpeg',
     'assets/images/introImage3.jpeg',
+  ];
+  List<String> introTextHeader = [
+    'SMARTER HOME MEANS SAFER HOME.',
+    'SMART STORE WITH GREAT PRICES.',
+    'SWITCH TO LOCKET NOW - WE\'LL COVER THE COST.',
+  ];
+  List<String> introParagraph = [
+    'Locket recognises this and rewards you with great value insurance for looking  after your home.',
+    'No smart tech? No Problem. You can still get Locket. And once you\'re a member, you can buy cool tech at amazing prices from a members-only smart store. ',
+    'If your old insuner changes cancellation fees, we\'ll pay you right back up to £250 in Amazon vouchers.',
   ];
 
   @override
@@ -101,17 +114,33 @@ class _MyBodyWidgetState extends State<_MyBodyWidget>
 
     controller =
         AnimationController(duration: const Duration(seconds: 3), vsync: this);
-    annimation = Tween(begin: 0.0, end: 1.0).animate(controller!)
-      ..addListener(() {
+    annimation = Tween(begin: 0.0, end: 1.0).animate(
+      controller!,
+    )..addListener(() {
         setState(() {});
       });
     controller!.forward();
+    print(widget.value);
+    if (widget.value == 3.0) {
+      controller1 = AnimationController(
+          duration: const Duration(seconds: 5), vsync: this);
+      annimation1 =
+          CurvedAnimation(parent: controller1!, curve: Curves.easeInOutBack);
+
+      controller1!.forward();
+    }
+
+    //_buttonController!.forward();
   }
 
   @override
   void dispose() {
     super.dispose();
     controller!.dispose();
+    if (widget.value == 3.0) {
+      controller1!.dispose();
+    }
+    //_buttonController!.dispose();
   }
 
   @override
@@ -122,7 +151,7 @@ class _MyBodyWidgetState extends State<_MyBodyWidget>
       appBar: StandardAppbar(
         onMenu: () {},
         onHelp: () {},
-        backgroundColor: widget.color,
+        backgroundColor: widget.value == 3.0 ? Color(0xFF20201F) : widget.color,
         appbarItemsColor: widget.appbarItemsColor,
       ),
       body: Center(
@@ -192,11 +221,11 @@ class _MyBodyWidgetState extends State<_MyBodyWidget>
               height: 20,
             ),
             Text(
-              'SMARTER HOME MEANS SAFER HOME.',
+              introTextHeader[currentImage],
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontStyle: FontStyle.italic,
-                fontSize: 40,
+                fontSize: 39,
                 color: widget.value == 3 ? Colors.white : Colors.black,
               ),
             ),
@@ -204,13 +233,49 @@ class _MyBodyWidgetState extends State<_MyBodyWidget>
               height: 20,
             ),
             Text(
-              'Locket recognises this and rewards you with great value insurance for looking  after your home.',
-              textAlign: TextAlign.justify,
+              introParagraph[currentImage],
+              textAlign: TextAlign.start,
               style: TextStyle(
                 fontSize: 18,
                 color: widget.value == 3 ? Colors.white : Colors.black,
               ),
-            )
+            ),
+            currentImage == 2
+                ? FadeTransition(
+                    opacity: annimation1!,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 25.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFFFFA142),
+                          onPrimary: Colors.white,
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40,
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed(LandingScreen.routeName);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 60.0, right: 60, top: 20, bottom: 20),
+                          child: Text(
+                            'CHECK MY PRICE',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                : SizedBox(),
           ],
         ),
       )),
